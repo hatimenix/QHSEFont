b7e1d01b5594174bdf7c771be95641c022c58291
bochra El hamri <dev7@paiperleck.lu>
2023-04-07 10:18:25 +0000

    crud processus

--- src/app/Services/Service-processus/processus.service.ts
+++ src/app/Services/Service-processus/processus.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Personnel } from 'src/app/models/Personnel';
 import { Processus } from 'src/app/models/Processus';
 import { environment } from 'src/environments/environment.development';
 
@@ -9,31 +10,35 @@ import { environment } from 'src/environments/environment.development';
 })
 export class ProcessusService {
   private API_Processus =environment.API_Processus;
+  private API_User =environment.API_User;
   constructor(private http: HttpClient) { }
 
   getProcessus(): Observable<Processus[]> {
     return this.http.get<Processus[]>(this.API_Processus);
   }
 
-  getOne(id: number): Observable<Processus> {
-    const url = `${this.API_Processus}/${id}`;
-    return this.http.get<Processus>(url);
+  
+  getProcessusById(id: number): Observable<Processus> {
+    return this.http.get<Processus>(`${this.API_Processus}${id}`);
   }
 
-  create(processus: Processus): Observable<Processus> {
+  
+  addProcessus(processus: Processus): Observable<Processus> {
     return this.http.post<Processus>(this.API_Processus, processus);
   }
+  
 
-  update(id: number, processus: Processus): Observable<Processus> {
-    const url = `${this.API_Processus}/${id}`;
+  
+  updateProcessus(id: number, processus: Processus): Observable<Processus> {
+    const url = `${this.API_Processus}${id}/`;
     return this.http.put<Processus>(url, processus);
   }
 
   deleteProcessus(id: number): Observable<void> {
-    if (!id || isNaN(id)) {
-      return throwError('Invalid ID provided.');
-    }
-    const url = `${this.API_Processus}${id}/`;
+    const url = `${this.API_Processus}${id}`;
     return this.http.delete<void>(url);
+  }
+  getPersonnelsList(): Observable<Personnel[]> {
+    return this.http.get<Personnel[]>(this.API_User);
   }
 }
