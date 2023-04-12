9e3a031bd0436badeefd7f46adf7101cacc56fc8
bochra El hamri <dev7@paiperleck.lu>
2023-04-12 13:36:35 +0000

    last updates bochra

--- src/app/Services/Service-personnel/personnel.service.ts
+++ src/app/Services/Service-personnel/personnel.service.ts
@@ -47,9 +47,28 @@ export class PersonnelService {
 
   
 
-  updatePersonnel(personnel: Personnel): Observable<Personnel> {
-    return this.http.put<Personnel>(`${this.API_URL_Utilisateur}${personnel.id}/`, personnel);
-  }
+  updatePersonnel(personnel: Personnel, image: File): Observable<Personnel> {
+    const formData = new FormData();
+    formData.append('id', personnel.id.toString());
+    formData.append('compte', personnel.compte);
+    formData.append('nom', personnel.nom);
+    formData.append('courrier', personnel.courrier);
+    formData.append('numero_tel', personnel.numero_tel);
+    formData.append('presente_vous', personnel.presente_vous);
+    formData.append('fonction', personnel.fonction);
+    formData.append('adresse_sip', personnel.adresse_sip);
+    formData.append('othermail', personnel.othermail);
+    if (image) {
+      formData.append('image', image, image.name);
+    }
+    return this.http.put<Personnel>(`${this.API_URL_Utilisateur}${personnel.id}/`, formData).pipe(
+      catchError((error) => {
+        console.error(error);
+        throw error;
+      })
+    );
+}
+
   updatePersonnelFormdata(formData: FormData): Observable<any> {
     const id = formData.get('id');
     const url = `${this.API_URL_Utilisateur}${id}/`;
