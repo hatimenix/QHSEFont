56975a8602b1424a2ee1286c0f4abd3f0123043b
PaiperleckMaroc <dev.maroc@paiperleck.lu>
2023-04-21 17:31:56 +0200

    last modifications ouchene

--- src/app/models/realisations.ts
+++ src/app/models/realisations.ts
@@ -1,7 +1,14 @@
+
 export class Realisations {
     id !: number;
     action_associe !: number;
-    action_realisee !: string;
+    action_realise !: string;
     date_realisation !: Date;
     etat !: string;
+
+    constructor(action_realise:string, date_realisation:Date, etat:string) {
+        this.action_realise = action_realise;
+        this.date_realisation = date_realisation;
+        this.etat = etat;
+    }
 }
