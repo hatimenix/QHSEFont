30ef687c4606153d325516f2a13159fa4c78efa5
Ilyas-Blal <dev9@paiperleck.lu>
2023-05-02 11:18:15 +0100

    RGPD

--- src/app/Components/RGPD/add-registre-traitement/add-registre-traitement.component.ts
+++ src/app/Components/RGPD/add-registre-traitement/add-registre-traitement.component.ts
@@ -13,8 +13,8 @@ import { FournisseurService } from 'src/app/Services/Service-fournisseurs/fourni
 export class AddRegistreTraitementComponent implements OnInit {
   fournisseurs: any[] = [];
   traitements: any[] = [];
-  isAllMtypedemesuredesecuriteEmpty: boolean = false;
-  @ViewChild('successModal', { static: true }) successModal:any;
+
+    @ViewChild('successModal', { static: true }) successModal:any;
   modalRef!: BsModalRef;
   constructor(private   traitementservice : ServiceRegistreTraitementService , private router : Router, private   fournisseurservice : FournisseurService,private bsModalService: BsModalService){}
 
@@ -23,80 +23,65 @@ export class AddRegistreTraitementComponent implements OnInit {
   traitementf = {
     id: 1,
     fournisseur: '',
-    REFtraitement: '',
+    typeregistre:'',
     nomtraitement: '',
+    description_generale:'',
     datedecreation: '',
     datedemiseajour: '',
-    donneesensible: '',
-    personneconcernee: '',
-    precision: '',
-    typeregistre: '',
+    responsable_traitement:'',
     finaliteprincipale: '',
     sous_finalite1: '',
     sous_finalite2: '',
     sous_finalite3: '',
     sous_finalite4: '',
+    donneesensible:'',
+    type_donnee:'',
     categorie: '',
     description: '',
     dureedeconcesrvation: '',
+    personneconcernees:'',
+    precisions:'',
+    typedestinataire:'',
+    precision:'',
+    donneeconcernees:'',
     mtypedemesuredesecurite: '',
-    precisiondumesuredesecurite: '',
-    typedestinataire: '',
-    precisions: '',
-    donneeconcernee: '',
     destinataire: '',
     pays: '',
     typedegarantie: '',
     lienversladocumentation: '',
-    lesdonneesconcernee: '',
-    prenomnomresptraitement: '',
-    emailresptraitement: '',
-    telephonereesptraitement: '',
-    prenommomDPO: '',
-    EmailDPO: '',
-    telephoneDPO: '',
-    prenomnomrepresentant: '',
-    emailrepresentant: '',
-    telephonerepresentant: ''
+    lesdonneesconcernee: ''
   };
     submitted = false;
     form = new FormGroup({
       fournisseur: new FormControl(''),
-      REFtraitement: new FormControl(''),
+      typeregistre: new FormControl(''),
       nomtraitement: new FormControl(''),
+      description_generale: new FormControl(''),
       datedecreation: new FormControl(''),
       datedemiseajour: new FormControl(''),
-      donneesensible: new FormControl(''),
-      personneconcernee: new FormControl(''),
-      precision: new FormControl(''),
-      typeregistre: new FormControl(''),
+      responsable_traitement: new FormControl(''),
       finaliteprincipale: new FormControl(''),
       sous_finalite1: new FormControl(''),
       sous_finalite2: new FormControl(''),
       sous_finalite3: new FormControl(''),
       sous_finalite4: new FormControl(''),
+      donneesensible: new FormControl(''),
+      type_donnee: new FormControl(''),
       categorie: new FormControl(''),
       description: new FormControl(''),
       dureedeconcesrvation: new FormControl(''),
-      mtypedemesuredesecurite: new FormControl(''),
-      precisiondumesuredesecurite: new FormControl(''),
-      typedestinataire: new FormControl(''),
+      personneconcernees: new FormControl(''),
       precisions: new FormControl(''),
-      donneeconcernee: new FormControl(''),
+      typedestinataire: new FormControl(''),
+      precision: new FormControl(''),
+      donneeconcernees: new FormControl(''),
+      mtypedemesuredesecurite: new FormControl(''),      
       destinataire: new FormControl(''),
       pays: new FormControl(''),
       typedegarantie: new FormControl(''),
       lienversladocumentation: new FormControl(''),
-      lesdonneesconcernee: new FormControl(''),
-      prenomnomresptraitement: new FormControl(''),
-      emailresptraitement: new FormControl(''),
-      telephonereesptraitement: new FormControl(''),
-      prenommomDPO: new FormControl(''),
-      EmailDPO: new FormControl(''),
-      telephoneDPO: new FormControl(''),
-      prenomnomrepresentant: new FormControl(''),
-      emailrepresentant: new FormControl(''),
-      telephonerepresentant: new FormControl('')
+      lesdonneesconcernee: new FormControl('')
+
 
     });
   ngOnInit(): void {
@@ -130,47 +115,39 @@ export class AddRegistreTraitementComponent implements OnInit {
         console.log(error); // Handle error
       }
     );
-    this.isAllMtypedemesuredesecuriteEmpty = this.traitements.every(traitement => !traitement.mtypedemesuredesecurite);
 
   }
   createTraitement() {
     const formData = new FormData();
     formData.append('fournisseur', this.traitementf.fournisseur);
-    formData.append('REFtraitement', this.traitementf.REFtraitement);
+    formData.append('typeregistre', this.traitementf.typeregistre);
     formData.append('nomtraitement', this.traitementf.nomtraitement);
     formData.append('datedecreation', this.traitementf.datedecreation);
     formData.append('datedemiseajour', this.traitementf.datedemiseajour);
-    formData.append('donneesensible', this.traitementf.donneesensible);
-    formData.append('personneconcernee', this.traitementf.personneconcernee);
+    formData.append('description_generale', this.traitementf.description_generale);
+    formData.append('responsable_traitement', this.traitementf.responsable_traitement);
     formData.append('precision', this.traitementf.precision);
-    formData.append('typeregistre', this.traitementf.typeregistre);
     formData.append('finaliteprincipale', this.traitementf.finaliteprincipale);
     formData.append('sous_finalite1', this.traitementf.sous_finalite1);
     formData.append('sous_finalite2', this.traitementf.sous_finalite2);
     formData.append('sous_finalite3', this.traitementf.sous_finalite3);
     formData.append('sous_finalite4', this.traitementf.sous_finalite4);
+    formData.append('donneesensible', this.traitementf.donneesensible);
+    formData.append('type_donnee', this.traitementf.type_donnee);
     formData.append('categorie', this.traitementf.categorie);
     formData.append('description', this.traitementf.description);
     formData.append('dureedeconcesrvation', this.traitementf.dureedeconcesrvation);
-    formData.append('mtypedemesuredesecurite', this.traitementf.mtypedemesuredesecurite);
-    formData.append('precisiondumesuredesecurite', this.traitementf.precisiondumesuredesecurite);
-    formData.append('typedestinataire', this.traitementf.typedestinataire);
+    formData.append('personneconcernees', this.traitementf.personneconcernees);
     formData.append('precisions', this.traitementf.precisions);
-    formData.append('donneeconcernee', this.traitementf.donneeconcernee);
+    formData.append('typedestinataire', this.traitementf.typedestinataire);
+    formData.append('precision', this.traitementf.precision);
+    formData.append('donneeconcernees', this.traitementf.donneeconcernees);
+    formData.append('mtypedemesuredesecurite', this.traitementf.mtypedemesuredesecurite);
     formData.append('destinataire', this.traitementf.destinataire);
     formData.append('pays', this.traitementf.pays);
     formData.append('typedegarantie', this.traitementf.typedegarantie);
     formData.append('lienversladocumentation', this.traitementf.lienversladocumentation);
     formData.append('lesdonneesconcernee', this.traitementf.lesdonneesconcernee);
-    formData.append('prenomnomresptraitement', this.traitementf.prenomnomresptraitement);
-    formData.append('emailresptraitement', this.traitementf.emailresptraitement);
-    formData.append('telephonereesptraitement', this.traitementf.telephonereesptraitement);
-    formData.append('prenommomDPO', this.traitementf.prenommomDPO);
-    formData.append('EmailDPO', this.traitementf.EmailDPO);
-    formData.append('telephoneDPO', this.traitementf.telephoneDPO);
-    formData.append('prenomnomrepresentant', this.traitementf.prenomnomrepresentant);
-    formData.append('emailrepresentant', this.traitementf.emailrepresentant);
-    formData.append('telephonerepresentant', this.traitementf.telephonerepresentant);
 
     this.traitementservice.create(formData).subscribe({
 
@@ -189,12 +166,28 @@ export class AddRegistreTraitementComponent implements OnInit {
 
 
 }
-onSelectionChange() {
-  this.isAllMtypedemesuredesecuriteEmpty = !this.traitementf.mtypedemesuredesecurite;
-}
+
 onCancel() {
   this.mode = 'list';
 }
+ isTypededonneeEmpty(): boolean {
+  return !this.traitements || this.traitements.length === 0 || this.traitements.every(traitement => !traitement.type_donnee);
+}
+
+isPersonneconcerneesEmpty(): boolean {
+  return !this.traitements || this.traitements.length === 0 || this.traitements.every(traitement => !traitement.personneconcernees);
+}
+
+isTypedesinataireEmpty(): boolean {
+  return !this.traitements || this.traitements.length === 0 || this.traitements.every(traitement => !traitement.typedestinataire);
+}
+isAllMtypedemesuredesecuriteEmpty(): boolean {
+  return !this.traitements || this.traitements.length === 0 || this.traitements.every(traitement => !traitement.mtypedemesuredesecurite);
+}
+isDestinataireEmpty(): boolean {
+  return !this.traitements || this.traitements.length === 0 || this.traitements.every(traitement => !traitement.destinataire);
+}
+
 
 
 get f() {
