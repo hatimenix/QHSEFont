4ed74347df9ff7dda18e935d33e628dc3566eb69
ElhamriBochra <dev7@paiperleck.lu>
2023-07-03 12:44:01 +0100

    modif css

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -9,12 +9,17 @@ import { Nc } from 'src/app/models/nc';
 })
 export class HomeComponent {
   icons = [
-    { link: '/listP', color: 'card-green', iconClass: 'fa fa-users fa-3x', title: 'Personnels' },
-    { link: '/list-registre-traitement', color: 'card-orange', iconClass: 'fa fa-lock fa-3x', title: 'RGPD' },
-    { link: '/nc-add', color: 'card-red', iconClass: 'fa fa-exclamation-circle fa-3x', title: 'Ouvrir une NC' },
-    { link: '/listdocument', color: 'card-brown', iconClass: 'fas fa-file-alt fa-3x', title: 'Documentation' },
-    { link: '#', color: 'card-purple', iconClass: 'fas fa-chart-pie fa-3x', title: 'Vision 360' },
-    { link: '#', color: 'card-black', iconClass: 'fa fa-cogs fa-3x', title: 'Paramètres' }
+    { link: '/listP', color: 'card-icon', imageSrc: 'assets/images/business-people.png', title: 'Personnels' },
+    { link: '/list-registre-traitement', color: 'card-icon', imageSrc: 'assets/images/rgpd.png', title: 'RGPD' },
+    { link: '/nc-add', color: 'card-icon', imageSrc: 'assets/images/liste-de-controle.png', title: 'Ouvrir une NC' },
+    { link: '/listdocument', color: 'card-icon', imageSrc: 'assets/images/doc.png', title: 'Documentation' },
+    { link: '/cartographie', color: 'card-icon',imageSrc: 'assets/images/reseau.png', title: 'Cartographie' },
+    { link: '#', color: 'card-icon', imageSrc: 'assets/images/parametre.png', title: 'Equipements' },
+    { link: '#', color: 'card-icon', imageSrc: 'assets/images/diner.png', title: 'Commande repas' },
+    { link: '#', color: 'card-icon', imageSrc: 'assets/images/restauration.png', title: 'Menus restaurants' },
+    { link: '#', color: 'card-icon', imageSrc: 'assets/images/des-documents.png', title: 'Fiches Techniques' },
+    { link: '#', color: 'card-icon', imageSrc: 'assets/images/health-and-care.png', title: 'Santé mentale' },
+    
   ];
   currentSlide = 0;
 
@@ -43,16 +48,35 @@ getNcs() {
   );
 }
 changeSlide(direction: number) {
-  const length = this.icons.length;
-  const slidesToShow = 6;
+  const totalCards = this.icons.length;
+  const cardsPerPage = 8; // Number of cards to display per page
 
+  // Calculate the total number of slides
+  const totalSlides = Math.ceil(totalCards / cardsPerPage);
+
+  // Calculate the new currentSlide based on the direction
   this.currentSlide += direction;
 
-  if (this.currentSlide < 0) {
-    this.currentSlide = length - slidesToShow;
-  } else if (this.currentSlide > length - slidesToShow) {
+  // Wrap around to the first or last slide if necessary
+  if (this.currentSlide >= totalSlides) {
     this.currentSlide = 0;
+  } else if (this.currentSlide < 0) {
+    this.currentSlide = totalSlides - 1;
   }
 }
+
+getVisibleIcons(): any[] {
+  const cardsPerPage = 8; // Number of cards to display per page
+  const start = this.currentSlide * cardsPerPage;
+  const end = start + cardsPerPage;
+
+  return this.icons.slice(start, end);
+}
+
+
+
+
+
+
 }
 
