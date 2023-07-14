df652bae4a73b98a005d1b365940d2902c78d942
ElhamriBochra <dev7@paiperleck.lu>
2023-07-14 11:21:07 +0100

    modifications modals/buttons 2n part

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -26,14 +26,8 @@ export class NavbarComponent {
   constructor(private authService: AuthService, private router: Router, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    this.getUserDetails();
-   
-  
-    
-     
-    
+    this.getUserDetails(); 
   }
-  
   
   getUserDetails(): void {
     const userImagePath = localStorage.getItem('userImagePath');
@@ -49,7 +43,7 @@ export class NavbarComponent {
         } else {
           this.user.image = ''; // Assign a default value when userImagePath is null
         }
-  
+
         // Save the image path to local storage
         localStorage.setItem('userImagePath', userImagePath || '');
       },
@@ -59,10 +53,6 @@ export class NavbarComponent {
     );
   }
   
-  
-  
-  
-
 
   onLogout(): void {
     this.authService.logout();
@@ -70,8 +60,5 @@ export class NavbarComponent {
     this.user = null;
     this.router.navigate(['/']);
   }
-
-  
-  
   
 }
