05c93db569223d4609f481eaf933ac49649e8c1a
ElhamriBochra <dev7@paiperleck.lu>
2023-08-10 10:52:06 +0100

    last modifications

--- src/app/Components/ResetPass/reset-pass/reset-pass.component.ts
+++ src/app/Components/ResetPass/reset-pass/reset-pass.component.ts
@@ -10,21 +10,38 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ResetPassComponent implements OnInit{
   
-
   token!: string;
-  newPassword!: string;
-  confirmPassword!: string;
+  newPassword: string = '';
+  confirmPassword: string = '';
+  passwordResetForm!: FormGroup;
   passwordResetSuccess: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
-    console.log(this.route.snapshot.params); // Check the entire params object
-    this.token = this.route.snapshot.params['token']; // Use square brackets notation here
-    console.log('Token:', this.token); // Check the extracted token value
+    this.token = this.route.snapshot.params['token'];
+
+    this.passwordResetForm = this.fb.group({
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get formControls() {
+    return this.passwordResetForm.controls;
   }
 
   resetPassword() {
+    if (this.passwordResetForm.invalid) {
+      console.error('Form is invalid. Please check the fields.');
+      return;
+    }
+
     if (this.newPassword === this.confirmPassword) {
       this.http.post<any>(`http://localhost:8001/api/reset-password/${this.token}/`, { new_password: this.newPassword }).subscribe(
         () => {
@@ -40,9 +57,8 @@ export class ResetPassComponent implements OnInit{
   }
 
   redirectToLogin() {
-    this.router.navigate(['/login']); // Redirect to the login page after successful password reset
+    this.router.navigate(['/login']);
   }
-
   }
 
 
